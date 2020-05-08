12f507022a6162b55fa9f3215054e3293e5f27e0
Bayo Ade <bayo.300@gmail.com>
2020-05-08 08:18:50 +0100

    docs

--- functions.py
+++ functions.py
@@ -24,3 +24,58 @@ def fibo(n):
     return res
 
 print(fibo(10))
+
+# default argument values
+
+def ask_ok(prompt, retries = 4, complaint = 'Yes or no, please!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries -1
+        if retries < 0:
+            raise IOError('uncooperative user')
+        print (complaint)
+
+xx = 5
+
+def f(arg = xx):
+    print(arg)
+xx = 6
+
+f()
+
+def fun(a, L=[]):
+    L.append(a)
+    return L
+
+
+print(fun(3))
+
+# keyword argument
+def parrot(voltage, state ='a stiff', action ='voom', type='Norwegian Blue'):
+    print('-- This parrot wouldn\'t', action, end=' ')
+    print('if you put', voltage, 'volts through it')
+    print('Lovely plumage, the', type)
+    print('-- It\'s', state, '!')
+
+parrot(1000)
+parrot(voltage=20000, action='VROOOOOOMYYY', state='sexy', type='Black Steiner' )
+
+# final formal parameter **name
+
+def cheeseshop(kind, *arguments, **keywords):
+    print('--Do you have any', kind, '?')
+    print('--I\'m sorry, we\'re all out of', kind)
+    for arg in arguments:
+        print(arg)
+    print('-' * 40)
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ':', keywords[kw])
+
+cheeseshop('Limburger', 'It\'s very runny, sir', shopkeeper='Michael Palin', client='John Cleese', sketch='Cheese Shop Sketch')
+
+
